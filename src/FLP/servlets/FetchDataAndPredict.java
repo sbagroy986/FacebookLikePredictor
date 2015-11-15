95f3aa7fbd25bf6dec5415b2a6572f5c3315a29e
Shrey <shrey14099@iiitd.ac.in>
2015-11-16 01:35:26 +0530

    Separating training dataset

--- src/FLP/servlets/FetchDataAndPredict.java
+++ src/FLP/servlets/FetchDataAndPredict.java
@@ -110,6 +110,19 @@ public class FetchDataAndPredict extends HttpServlet {
 			System.out.println("Invalid JSON Object!");
 		}
 	    
+	    ArrayList<Post> train_dataset = new ArrayList<>();
+	    Collections.shuffle(posts);
+	    int count_posts = 0;
+	    for(int i=0;count_posts < 0.2*posts.size();i++)
+	    {
+	    	if(posts.get(i).getRawData().has("message") || posts.get(i).getRawData().has("caption") || posts.get(i).getRawData().has("description"))
+	    	{
+	    		train_dataset.add(posts.get(i));
+	    		posts.remove(i);
+	    		count_posts++;
+	    	}
+	    }
+	    
 	    try {
 			LinearRegression.setLinearRegression(posts,response,token);
 		} catch (JSONException e) {
@@ -118,9 +131,6 @@ public class FetchDataAndPredict extends HttpServlet {
 	    LinearRegression.display(posts,response);
 	  
 	    HashMap<String,Integer> top10 = LinearRegression.getTop10();
-
-	    
-	    Collections.shuffle(posts);
 	    
 	    response.getWriter().print("<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><meta name=\"description\" content=\"\"><meta name=\"author\" content=\"\"><title>Results &#8226; AP Project</title><link rel=\"shortcut icon\" href=\"http://www.google.com/s2/favicons?domain=www.iiitd.ac.in\" type=\"image/x-icon\"><!-- Bootstrap Core CSS --><link href=\"./assets/css/bootstrap.min.css\" rel=\"stylesheet\"><!-- Custom CSS --><link href=\"./assets/css/small-business.css\" rel=\"stylesheet\"><!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --><!-- WARNING: Respond.js doesn't work if you view the page via file:// --><!--[if lt IE 9]><script src=\"https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js\"></script><script src=\"https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js\"></script><![endif]--></head><body><!-- Page Content -->    <div class='container'> <div class='row'><div class='col-md-12'>    <h1>Procedure</h1>    <p>Implemented a basic Machine Learning algorithm to predict likes on a post, using the previous posts of a user.Since the algorithm had to be self-implemented (i.e, we could not use third-party ML libraries), we have used simple Linear Regression for this project.</p>     		<p style='text-align:center;'>h<sub>&#920;</sub>(x) = &#920;<sub>0</sub> + &#920;<sub>1</sub>x<sub>1</sub> + &#920;<sub>2</sub>x<sub>2</sub> + .... + &#920;<sub>n</sub>x<sub>n</sub> = X&#920;<sup>T</sup></p>     		<p>Here, h<sub>&#920;</sub> is our hypothesis function. X is a mxn matrix where m is the number of data samples (number of posts, in this case) and n is the number of features. &#920; is a 1xn matrix which contains parameters (&#920;<sub>1</sub>,&#920;<sub>2</sub>...&#920;<sub>n</sub>) corresponding to each feature. This gives us h<sub>&#920;</sub>, an mx1 matrix which we cross-reference with the actual likes of the m corresponding posts to calculate accuracy.</p>     		<p>We are extracting 14 features for each post. This list is (by no means) exhaustive. It's possible to go into much more detail but since we're short on time, we'll just cover these basic features: </p>     		<div class='container'>     			<div class='row'>     				<div class='col-md-4'>			     		<ul>								<li>People in (with the user) the post</li>								<li>People tagged in the message</li>								<li>Source of post (Categorical/One Hot Encoded)</li>								<li>Presence of message</li>								<li>Presence of description</li>								<li>Presence of caption</li>								<li>Length of message</li>			     		</ul>     					     				</div>     				<div style='col-md-4'>			     		<ul>								<li>Presence of location</li>								<li>Type of post (Categorical/One Hot Encoded)</li>								<li>Type of status (Categorical/One Hot Encoded)</li>								<li>Created hour of day</li>								<li>Created day of week</li>								<li>Number of hashtags</li>								<li>Number of emotes</li>			     		</ul>     					     				</div>     			</div>     		</div></div> </div>  <hr><div class=\"container\"><!-- Heading Row --><div class=\"row\"><div class=\"col-md-8\"><h1>Results</h1><p>Using " + posts.size() +" posts obtained from the Facebook graph API, we give you the following results:</p><br/>");
 		
@@ -128,22 +138,19 @@ public class FetchDataAndPredict extends HttpServlet {
 	    Double[] accuracy = LinearRegression.getAccuracy();
 	    
 	    response.getWriter().println("<br/>            </div>            <div class='col-md-12' style='height:650px;'>                <div id='chart_div' style='height:600px;'></div>            </div><div class='col-md-6'> <div id='curve_chart' style='width: 650px; height: 500px'></div></div><div class='col-md-5' style='height:597px;'>  <ul class='nav nav-tabs list-inline' style='margin-top:55px'>  <li class='active'><a data-toggle='tab' href='#menu0'>3 likes</a></li>  <li><a data-toggle='tab' href='#menu1'>5 likes</a></li>  <li><a data-toggle='tab' href='#menu2'>10 likes</a></li>  <li><a data-toggle='tab' href='#menu3'>15 likes</a></li></ul><div class='tab-content'><div id='menu0' class='tab-pane fade active'>  <div id='donutchart-3' style='width: 700px; height: 500px;'></div></div><div id='menu1' class='tab-pane fade active'>  <div id='donutchart-5' style='width: 700px; height: 500px;'></div></div><div id='menu2' class='tab-pane fade active'>  <div id='donutchart-10' style='width: 700px; height: 500px;'></div></div><div id='menu3' class='tab-pane fade active'>  <div id='donutchart-15' style='width: 700px; height: 500px;'></div></div></div> </div> </div>       <hr>        <div class='row'>            <div class='col-lg-12'>                <div class='well text-center'>                    <div id='three' style=\"text-align:center\"> Accuracy for predicted likes within a 3 like margin : <b>" + new BigDecimal(accuracy[0] ).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue() + "%</b></div>                    <div id='five' style=\"text-align:center\"> Accuracy for predicted likes within a 5 like margin : <b>" + new BigDecimal(accuracy[1]).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue() + "%</b></div>                    <div id='ten' style=\"text-align:center\"> Accuracy for predicted likes within a 10 like margin : <b>" + new BigDecimal(accuracy[2] ).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue() + "%</b></div>                    <div id='fif' style=\"text-align:center\"> Accuracy for predicted likes within a 15 like margin : <b>" + new BigDecimal(accuracy[3] ).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue() + "%</b></div>                </div>            </div>        </div>        <div class='row' id='posts'>");
-	    int count_posts = 0;
-	    for(int i=1;count_posts<=15;i++)
+	    LinearRegression.testTheta(train_dataset);
+	    for(int i=0;i<train_dataset.size();i++)
 	    {
 			try {
-				if(posts.get(i).getRawData().has("message") || posts.get(i).getRawData().has("caption") || posts.get(i).getRawData().has("description"))
-				{
 					count_posts++;
-					response.getWriter().print("<div class='col-md-8'>    <div class='panel panel-default'>    	<div class='panel-heading'>    		<h5>Post ID: <a href='https://www.facebook.com/"+posts.get(i).getRawData().getString("id")+"'>"+ posts.get(i).getRawData().getString("id") + "</a></h5>    	</div>  <div class='panel-body'><p><img src='"+ pic_url+"' height='70px' width='70px;' class='img-circle pull-left' style='margin-right:20px; margin-bottom:1px;'> <a href='https://www.facebook.com/" + user_id  +"'>" + user_name + "</a></p>");
-					if(posts.get(i).getRawData().has("message"))
-						response.getWriter().print(posts.get(i).getRawData().getString("message"));
-					else if(posts.get(i).getRawData().has("caption"))
-						response.getWriter().print(posts.get(i).getRawData().getString("caption"));
-					else if(posts.get(i).getRawData().has("description"))
-						response.getWriter().print(posts.get(i).getRawData().getString("description"));
-					response.getWriter().print("<hr><div style='text-align:center'>Actual likes:&nbsp;"+ posts.get(i).getLikes() + "&nbsp;<img src='http://www.brandsoftheworld.com/sites/default/files/styles/logo-thumbnail/public/102011/like_icon.png?itok=nkurUMlZ' height='18px' width='18px' style='margin-bottom:6px;margin-left:2px;'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Predicted likes:&nbsp;" + posts.get(i).getPredictedLikes() + "&nbsp;<img src='https://image.freepik.com/free-icon/thumb-up-to-like-on-facebook_318-37196.png' height='18px' width='18px' style='margin-bottom:6px;margin-left:2px;'> </div>					</div>	     </div></div>");
-				}
+					response.getWriter().print("<div class='col-md-8'>    <div class='panel panel-default'>    	<div class='panel-heading'>    		<h5>Post ID: <a href='https://www.facebook.com/"+train_dataset.get(i).getRawData().getString("id")+"'>"+ train_dataset.get(i).getRawData().getString("id") + "</a></h5>    	</div>  <div class='panel-body'><p><img src='"+ pic_url+"' height='70px' width='70px;' class='img-circle pull-left' style='margin-right:20px; margin-bottom:1px;'> <a href='https://www.facebook.com/" + user_id  +"'>" + user_name + "</a></p>");
+					if(train_dataset.get(i).getRawData().has("message"))
+						response.getWriter().print(train_dataset.get(i).getRawData().getString("message"));
+					else if(train_dataset.get(i).getRawData().has("caption"))
+						response.getWriter().print(train_dataset.get(i).getRawData().getString("caption"));
+					else if(train_dataset.get(i).getRawData().has("description"))
+						response.getWriter().print(train_dataset.get(i).getRawData().getString("description"));
+					response.getWriter().print("<hr><div style='text-align:center'>Actual likes:&nbsp;"+ train_dataset.get(i).getLikes() + "&nbsp;<img src='http://www.brandsoftheworld.com/sites/default/files/styles/logo-thumbnail/public/102011/like_icon.png?itok=nkurUMlZ' height='18px' width='18px' style='margin-bottom:6px;margin-left:2px;'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Predicted likes:&nbsp;" + train_dataset.get(i).getPredictedLikes() + "&nbsp;<img src='https://image.freepik.com/free-icon/thumb-up-to-like-on-facebook_318-37196.png' height='18px' width='18px' style='margin-bottom:6px;margin-left:2px;'> </div>					</div>	     </div></div>");
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
